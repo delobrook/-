087daa05d6e1c59b15b468dd471b6e3db6d7bd48
Delano tracey <delanotracey@gmail.com>
2020-10-24 13:38:11 -0500

    javascript 10/24/2020

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 /* Add your JavaScript to this file */
+window.onload=function(){
+    var message=document.getElementsByClassName("message")[0];
+    var subscribe=document.getElementsByTagName("button")[0];
+    var email;
+    subscribe.setAttribute("type","button");
+    subscribe.onclick=function(){emailsubscribe()}
+        
+    function emailsubscribe(){
+        email=document.getElementById("email").value;
+        if(email==""){
+            message.textContent="please enter a valid email address"
+        }else{
+            message.textContent="Thank you! Your email address "+email+" has been added to our mailing list! ";
+        }
+    }
+}
